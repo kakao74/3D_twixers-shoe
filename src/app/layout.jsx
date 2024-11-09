e72a70a98e465905fbcbd58bd2be3c29698d92da
alijalloul <ali.z.jalloul@gmail.com>
2024-11-09 15:53:40 +0200

    added a Head component for google analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 import "./globals.css";
 
 export const metadata = {
@@ -8,6 +10,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-897K03VZG5"
+        ></Script>
+        <Script id="" strategy="lazyOnload">
+          {`  window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-897K03VZG5');`}
+        </Script>
+      </head>
+
       <body className="max-h-screen flex justify-center items-stretch ">
         {children}
       </body>
